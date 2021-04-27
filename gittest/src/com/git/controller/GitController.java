8ccb041bf9273a5c1979844ca530f3a8d5ad1758
tae-un1 <70499662+tae-un1@users.noreply.github.com>
2021-04-27 20:39:20 +0900

    #8 test2매소드추가 -tu-

--- gittest/src/com/git/controller/GitController.java
+++ gittest/src/com/git/controller/GitController.java
@@ -2,6 +2,10 @@ package com.git.controller;
 
 public class GitController {
 	public void main() {
-		System.out.println("추가한클래스");
+		System.out.println("추가한클래스 매소드");
+	}
+	
+	public void test2() {
+		System.out.println("원격으로 저장하자!");
 	}
 }
